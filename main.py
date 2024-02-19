7fa31bf9abcbf1a6a5318d9f50c32058689b7784
samuelColoradoCastrillon <sacolorado@unal.edu.co>
2024-02-19 17:51:28 -0500

    Second Submit

--- main.py
+++ main.py
@@ -36,15 +36,17 @@ class Auto:
         Auto.cantidadCreados += 1
 
     def cantidadAsientos(self):
+        cantidad = 0
         for asiento in self.asientos:
-            if not isinstance(asiento, Asiento):
-                break
-        return len(self.asientos)
+            if isinstance(asiento, Asiento):
+                cantidad += 0
+        return cantidad
     
     def verificarIntegridad(self):
         if self.registro != self.motor.registro:
             return "Las piezas no son originales"
         for asiento in self.asientos:
-            if self.registro != asiento.registro:
-                return "Las piezas no son originales"
+            if isinstance(asiento, Asiento):
+                if self.registro != asiento.registro:
+                    return "Las piezas no son originales"
         return "Auto original"
